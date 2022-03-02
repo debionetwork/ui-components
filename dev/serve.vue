aca834ce24afbd37b4701b29ea0ca11d56013588
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-03-02 23:44:19 +0700

    feat: ui debio avatar.

--- dev/serve.vue
+++ dev/serve.vue
@@ -13,6 +13,6 @@ export default Vue.extend({
 
 <template>
   <div id="app">
-    <debionetwork-ui-components-sample />
+    <ui-debio-avatar />
   </div>
 </template>
--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,2 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export { default as DebionetworkUiComponentsSample } from './debionetwork-ui-components-sample.vue';
+export { default as UiDebioAvatar } from './Avatar';
