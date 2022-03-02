5eeed0590103558c00fd03a4b0828fcd1f67b754
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-03-02 22:36:39 +0700

    feat: initial commit.

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { DebionetworkUiComponentsSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  DebionetworkUiComponentsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <debionetwork-ui-components-sample />
+  </div>
+</template>
